4248e5afcb70e3536eedb06a684576f0048fbd63
Shawn Adrian <shawn@nerdburn.com>
2014-12-15 15:30:25 -0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+// requirements
+var gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    less = require('gulp-less'),
+    path = require('path');
+    
+// run
+gulp.task('default', function() {
+  
+  // easier to read variable
+  var themeName = 'cur8r';
+  
+  // less
+  gulp.src('./assets/less/' + themeName + '.less')
+    .pipe(less({ paths: [path.join(__dirname, 'less', 'includes')]}))
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('./assets/css/'));
+  
+});
